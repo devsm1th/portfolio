e49bc99a9472bfad2616f96cf13f52c67bdb28af
devsm1th <devsm1thmail@proton.me>
2026-01-11 19:43:31 +0200

    initial commit

---
+++ src/data/techs.ts
@@ -0,0 +1,28 @@
+export const TECHS = {
+  reactjs: { name: 'ReactJS', href: 'https://reactjs.org', icon: 'tabler:brand-react', color: '#61DAFB' },
+  nextjs: { name: 'Next.js', href: 'https://nextjs.org', icon: 'tabler:brand-nextjs', color: '#000000' },
+  tailwindcss: {
+    name: 'Tailwind CSS',
+    href: 'https://tailwindcss.com',
+    icon: 'tabler:brand-tailwind',
+    color: '#06B6D4',
+  },
+  typescript: {
+    name: 'TypeScript',
+    href: 'https://www.typescriptlang.org',
+    icon: 'tabler:brand-typescript',
+    color: '#3178C6',
+  },
+  framer_motion: {
+    name: 'Framer Motion',
+    href: 'https://www.framer.com/motion/',
+    icon: 'tabler:brand-framer',
+    color: '#0055FF',
+  },
+  strapi: {
+    name: 'Strapi',
+    href: 'https://strapi.io/',
+    icon: 'tabler:database',
+    color: '#4945ff',
+  },
+};
